5a56897732fce7a713f342eeed0e8a5599d1b07f
Joseph70k <aprk0429@daum.net>
2025-05-12 15:53:32 -1000

    Add Displaying data part code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function MyApp() {
       <h1>Welcome to my app!</h1>
       <MyButton />
       <AboutPage />
+       <Profile />
     </div>
   );
 }
@@ -21,6 +22,29 @@ function AboutPage() {
     <>
       <h2>About</h2>
       <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
