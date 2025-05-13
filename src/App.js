eb0cffdf6470d9242f0d47ecdc473f86d6b73016
Joseph70k <aprk0429@daum.net>
2025-05-12 16:09:31 -1000

    Add Rendering lists

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function MyApp() {
       <MyButton />
       <AboutPage />
        <Profile />
+       <ShoppingList />
     </div>
   );
 }
@@ -47,4 +48,25 @@ function Profile() {
       />
     </>
   );
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return <ul>{listItems}</ul>;
 }
