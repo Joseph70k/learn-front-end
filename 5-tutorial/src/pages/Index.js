cf868cefbf69545b05fc0b09aee24bb7aef14f33
Joseph70k <aprk0429@daum.net>
2025-06-08 14:16:25 -1000

    Add css styles simply

--- 5-tutorial/src/pages/Index.js
+++ 5-tutorial/src/pages/Index.js
@@ -1,8 +1,18 @@
+import '../App.css';
+
 function Index() {
   return (
-    <div>
-      <h1>Welcome to BYU&Sale</h1>
-      <p>This is a simple buy-and-sell platform for BYU students.</p>
+    <div className="index-page">
+      <h1>Welcome to BYU&Sale!</h1>
+      <p>This is a simple buy-and-sell platform made using React.</p>
+
+      <section className="index-section">
+        <h2>Technologies Used</h2>
+        <ul>
+          <li>React</li>
+          <li>React Router DOM</li>
+        </ul>
+      </section>
     </div>
   );
 }
